4f38a72f1fa5ffa55f195e2e86b0ec9074877f2e
anser456t <anseranwaart@gmail.com>
2026-09-20 20:31:28 +0300

    Modified by www.SourceFiles.app

--- TestExtensions/src/02-storage-test/content.js
+++
@@ -1,6 +0,0 @@
-chrome.storage.local.get(["visitCount"]).then(function(result) {
-  var count = (result.visitCount || 0) + 1;
-  chrome.storage.local.set({ visitCount: count }).then(function() {
-    console.log("Trident Storage Test: visit count is now " + count);
-  });
-});
